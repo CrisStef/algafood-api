66dd88448f7b8bbf0ed0422e929047b6c60b5dde
Cris Stef <cristiane.souza1234@hotmail.com>
2021-03-12 23:22:15 -0300

    Curso especialista spring  rest

---
+++ src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.algaworks.algafood;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
