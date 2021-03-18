65420fa4b0ddd67300a71aee6e2fe2bf3dd94c6e
Cris Stef <cristiane.souza1234@hotmail.com>
2021-03-17 22:41:19 -0300

    Acessando propriedades com @ConfigurationProperties

--- src/main/java/com/algaworks/algafood/di/notification/EmailNotifier.java
+++ src/main/java/com/algaworks/algafood/di/notification/EmailNotifier.java
@@ -1,23 +1,22 @@
 package com.algaworks.algafood.di.notification;
 
+import com.algaworks.algafood.config.NotifierProperties;
 import com.algaworks.algafood.di.model.Customer;
 import com.algaworks.algafood.di.model.annotations.NotifierType;
 import com.algaworks.algafood.di.model.enums.LevelUrgency;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @NotifierType(LevelUrgency.NOT_URGENT)
 @Component
 public class EmailNotifier implements Notifier {
-	@Value("${notifier.email.host-server}")
-	private String host;
-
-	@Value("${notifier.email.host-server-port}")
-	private Integer port;
+	@Autowired
+    private NotifierProperties properties;
 
 	@Override
 	public void notify(Customer customer, String message) {
+        System.out.println(properties.getHostServer());
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", customer.getName(), customer.getEmail(), message);
 	}
 }
