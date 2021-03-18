b23883c330d9dffa8e975af6ad1b151767978803
Cris Stef <cristiane.souza1234@hotmail.com>
2021-03-17 22:20:50 -0300

    Criando e acessando propriedades customizadas com @Value

--- src/main/java/com/algaworks/algafood/di/notification/EmailNotifier.java
+++ src/main/java/com/algaworks/algafood/di/notification/EmailNotifier.java
@@ -4,17 +4,18 @@ import com.algaworks.algafood.di.model.Customer;
 import com.algaworks.algafood.di.model.annotations.NotifierType;
 import com.algaworks.algafood.di.model.enums.LevelUrgency;
 
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Profile("prod")
 @NotifierType(LevelUrgency.NOT_URGENT)
 @Component
 public class EmailNotifier implements Notifier {
-	public EmailNotifier() {
-		System.out.println("NotificadorEmail REAL");
-	}
-	
+	@Value("${notifier.email.host-server}")
+	private String host;
+
+	@Value("${notifier.email.host-server-port}")
+	private Integer port;
+
 	@Override
 	public void notify(Customer customer, String message) {
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", customer.getName(), customer.getEmail(), message);
