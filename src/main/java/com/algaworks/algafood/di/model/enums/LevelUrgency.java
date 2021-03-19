d17b80f46fe6c3f3aaed7bfbb8801434b070dd22
Cris Stef <cristiane.souza1234@hotmail.com>
2021-03-18 23:11:45 -0300

    Introdução a JPA e Hibernate

--- src/main/java/com/algaworks/algafood/di/model/enums/LevelUrgency.java
+++
@@ -1,6 +0,0 @@
-package com.algaworks.algafood.di.model.enums;
-
-public enum LevelUrgency {
-	URGENT,
-	NOT_URGENT
-}
---
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurant.java
@@ -0,0 +1,70 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Restaurant {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	@Column(name = "freight_rate")
+	private BigDecimal freightRate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getFreightRate() {
+		return freightRate;
+	}
+
+	public void setFreightRate(BigDecimal freightRate) {
+		this.freightRate = freightRate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Restaurant other = (Restaurant) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
