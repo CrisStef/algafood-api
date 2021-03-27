020f1e81d874ce3f74ca30ed179acc51d26bc0c9
Cris Stef <cristiane.souza1234@hotmail.com>
2021-03-26 22:28:50 -0300

    Post Put e Delete

--- src/main/java/com/algaworks/algafood/api/controller/KitchenController.java
+++ src/main/java/com/algaworks/algafood/api/controller/KitchenController.java
@@ -6,14 +6,20 @@ import com.algaworks.algafood.api.model.KitchenXmlWrapper;
 import com.algaworks.algafood.domain.model.Kitchen;
 import com.algaworks.algafood.domain.repository.KitchenRepository;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,15 +41,34 @@ public class KitchenController {
 	@GetMapping("/{kitchen_id}")
 	public ResponseEntity<Kitchen> find(@PathVariable("kitchen_id") Long id) {
 		Kitchen kitchen = kitchenRepository.findById(id);
+		return ResponseEntity.ok().body(kitchen);
+	}
 
-		//return ResponseEntity.status(HttpStatus.OK).body(kitchen);
-		//return ResponseEntity.ok().body(kitchen);
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Kitchen save(@RequestBody Kitchen kitchen) {
+		return kitchenRepository.save(kitchen);
+	}
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/kitchens");
-		return ResponseEntity
-				.status(HttpStatus.FOUND)
-				.headers(headers)
-				.build();
+	@PutMapping("/{kitchen_id}")
+	public ResponseEntity<Kitchen> update(@PathVariable("kitchen_id") Long id, @RequestBody Kitchen kitchen) {
+		Kitchen currentKitchen = kitchenRepository.findById(id);
+		BeanUtils.copyProperties(kitchen, currentKitchen, "id");
+		kitchenRepository.save(currentKitchen);
+		return ResponseEntity.ok().body(currentKitchen);
+	}
+
+	@DeleteMapping("/{kitchen_id}")
+	public ResponseEntity<Kitchen> remove(@PathVariable("kitchen_id") Long id) {
+		try {
+			Kitchen kitchen = kitchenRepository.findById(id);
+			if (kitchen != null) {
+				kitchenRepository.remove(kitchen);
+				return ResponseEntity.noContent().build();
+			}
+			return ResponseEntity.notFound().build();
+		} catch (DataIntegrityViolationException e) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
 	}
 }
