72ad90e96751ca91c2eeb98668a5728db6efd40b
Cris Stef <cristiane.souza1234@hotmail.com>
2021-04-01 17:14:06 -0300

    JPQL para um arquivo XML

--- src/main/java/com/algaworks/algafood/domain/repository/RestaurantRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestaurantRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 import com.algaworks.algafood.domain.model.Restaurant;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +13,5 @@ import org.springframework.stereotype.Repository;
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 	public List<Restaurant> findByFreightRateBetween(BigDecimal initialFraightRate, BigDecimal finalFraightRate);
 
-	@Query("FROM Restaurant WHERE name LIKE %:name% AND kitchen.id = :id")
 	public List<Restaurant> findByNameAndKitchenId(String name, @Param("id") Long kitchenId);
-	//public List<Restaurant> findByNameContainingAndKitchenId(String name, Long kitchenId);
 }
