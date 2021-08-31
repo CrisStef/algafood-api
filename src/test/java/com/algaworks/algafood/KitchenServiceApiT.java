15ace971675224de135849829c3b71bff0f66041
Cristiane Santana <cristiane.souza1234@hotmail.com>
2021-08-30 23:22:59 -0300

    Criando banco de testes e usando @TestPropertySource

--- src/test/java/com/algaworks/algafood/KitchenServiceApiT.java
+++ src/test/java/com/algaworks/algafood/KitchenServiceApiT.java
@@ -3,14 +3,21 @@ package com.algaworks.algafood;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasSize;
 
+import com.algaworks.algafood.domain.model.Kitchen;
+import com.algaworks.algafood.domain.repository.KitchenRepository;
+import com.algaworks.algafood.util.DatabaseCleaner;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.hasItems;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.restassured.RestAssured;
@@ -18,16 +25,26 @@ import io.restassured.http.ContentType;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class KitchenServiceApiT {
 
 	@LocalServerPort
 	private int port;
 
+	@Autowired
+	private DatabaseCleaner databaseCleaner;
+
+	@Autowired
+	private KitchenRepository kitchenRepository;
+
 	@Before
 	public void setUp() {
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails(); /** */
 		RestAssured.port = port;
 		RestAssured.basePath = "/kitchens";
+
+		databaseCleaner.clearTables();
+		this.insertData();
 	}
 
 	@Test
@@ -47,7 +64,40 @@ public class KitchenServiceApiT {
 		.when()
 			.get()
 		.then()
-			.body("", hasSize(4))
-			.body("name", hasItems("Indiana", "Tailandesa"));
+			.body("", hasSize(2))
+			.body("name", hasItems("Brasileira", "Tailandesa"));
+	}
+
+	@Test
+	public void returnStatusOkWhenGetKitchenExistsTest() {
+		given()
+			.pathParam("kitchenId", 2)
+			.accept(ContentType.JSON)
+		.when()
+			.get("/{kitchenId}")
+		.then()
+			.statusCode(HttpStatus.OK.value())
+			.body("name", equalTo("Brasileira"));
+	}
+
+	@Test
+	public void returnStatusNotFoundWhenGetKitcheNonexistentTest() {
+		given()
+			.pathParam("kitchenId", 100)
+			.accept(ContentType.JSON)
+		.when()
+			.get("/{kitchenId}")
+		.then()
+			.statusCode(HttpStatus.NOT_FOUND.value());
+	}
+
+	private void insertData() {
+		Kitchen kitchen1 = new Kitchen();
+		kitchen1.setName("Tailandesa");
+		kitchenRepository.save(kitchen1);
+
+		Kitchen kitchen2 = new Kitchen();
+		kitchen2.setName("Brasileira");
+		kitchenRepository.save(kitchen2);
 	}
 }
