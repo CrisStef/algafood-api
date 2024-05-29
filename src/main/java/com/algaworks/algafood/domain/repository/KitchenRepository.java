035fbc1d0da1cf89cc4e79a12ad5bdcdecdc2430
Cristiane Santana <cristiane.santana@pasqualisolution.com.br>
2024-05-28 23:12:58 -0300

    12.11. Regra de negócio para evitar usuários com e-mails duplicados
    usando detach do jpa

--- src/main/java/com/algaworks/algafood/domain/repository/KitchenRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/KitchenRepository.java
@@ -4,10 +4,9 @@ import java.util.List;
 
 import com.algaworks.algafood.domain.model.Kitchen;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface KitchenRepository extends JpaRepository<Kitchen, Long> {
+public interface KitchenRepository extends CustomJpaRepository<Kitchen, Long> {
 	List<Kitchen> findAllByNameContaining(String name);
 }
