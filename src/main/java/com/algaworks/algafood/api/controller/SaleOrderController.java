45912d49cc44e21eb854cce4e32898593b36e3dc
Cristiane Santana <cristiane.souza1234@hotmail.com>
2024-06-19 23:53:59 -0300

    13.3. Limitando os campos retornados pela API com Squiggly

--- src/main/java/com/algaworks/algafood/api/controller/SaleOrderController.java
+++ src/main/java/com/algaworks/algafood/api/controller/SaleOrderController.java
@@ -4,18 +4,15 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.json.MappingJacksonValue;
 
 import com.algaworks.algafood.api.model.request.SaleOrderRequest;
 import com.algaworks.algafood.api.model.response.SaleOrderListResponse;
@@ -23,8 +20,6 @@ import com.algaworks.algafood.api.model.response.SaleOrderResponse;
 import com.algaworks.algafood.domain.exception.BusinessException;
 import com.algaworks.algafood.domain.exception.EntityNotFoundException;
 import com.algaworks.algafood.domain.service.SaleOrderService;
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 @RequestMapping("/saleOrders")
@@ -32,26 +27,9 @@ public class SaleOrderController {
 	@Autowired
 	private SaleOrderService saleOrderService;
 
-	// @GetMapping
-	// public List<SaleOrderListResponse> findAll() {
-	// 	return saleOrderService.findAll();
-	// }
-
 	@GetMapping
-	public MappingJacksonValue findAll(@RequestParam(required = false) String fields) {
-		List<SaleOrderListResponse> saleOrderListResponse = saleOrderService.findAll();
-		MappingJacksonValue saleOrderWrapper = new MappingJacksonValue(saleOrderListResponse);
-
-		SimpleFilterProvider filterProvider = new SimpleFilterProvider();
-		filterProvider.addFilter("saleOrderFilter", SimpleBeanPropertyFilter.serializeAll());
-
-		if (StringUtils.isNotBlank(fields)) {
-			filterProvider.addFilter("saleOrderFilter", SimpleBeanPropertyFilter.filterOutAllExcept(fields.split(",")));
-		}
-
-		saleOrderWrapper.setFilters(filterProvider);
-
-		return saleOrderWrapper;
+	public List<SaleOrderListResponse> findAll() {
+		return saleOrderService.findAll();
 	}
 
 	@GetMapping("/{sale_order_code}")
